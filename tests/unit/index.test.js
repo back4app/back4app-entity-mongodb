1a0351e983d35cbb30cdea8a0e54d532041beb1e
Seiji Akiyama <seiji.akiyama2.0@gmail.com>
2015-10-19 18:49:07 -0200

    Before Merge.

--- tests/unit/index.test.js
+++ tests/unit/index.test.js
@@ -2,9 +2,7 @@
 
 var expect = require('chai').expect;
 var mongoose = require('mongoose');
-var Entity =
-  require('../../node_modules/@back4app/back4app-entity').models.Entity;
-var Schema = require('../../src/back/models/schema');
+var Entity = require('@back4app/back4app-entity').models.Entity;
 
 describe('index', function () {
   var db;
@@ -12,6 +10,10 @@ describe('index', function () {
   before(function () {
     mongoose.connect('mongodb://localhost/test');
     db = mongoose.connection;
+  });
+
+  after(function () {
+    mongoose.disconnect();
   });
 
   it('expect to connect with mongoose', function (done) {
@@ -40,31 +42,6 @@ describe('index', function () {
     var person = new Person();
     person.name = 'John';
     expect(Person.methods.greeting.call(person), 'I am John');
-  });
-
-  it('expect to create new schema', function () {
-    var Person = Entity.specify({
-      name: 'Person',
-      attributes: {
-        name: {
-          type: 'String',
-          multiplicity: '1',
-          default: undefined
-        }
-      },
-      methods: {
-        greeting: function greeting() {
-          return 'I am ' + this.name;
-        }
-      }
-    });
-
-    var PersonModel = Schema.buildModel(Person);
-
-    console.log(new PersonModel({
-      name: 'John'
-    }));
-
   });
 
 });
